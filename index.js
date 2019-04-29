e673ed5d4c09b3b4c81c58f2b3cbf4c9cedf1547
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-04-29 16:54:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // add solution here
+let result = [];
+
+const theBeatlesPlay = function(musicians, instruments){
+  for (let i=0; i<musicians.length; i++){
+    const curMusician = musicians[i]
+    
+    for (let j=i; j<instruments.length; j++){
+      const curInstrument = instruments[j]
+      
+      result.push((`${curMusician} plays ${curInstrument}`))
+      break;
+    }
+  }
+  return result;
+}
+
+
+let result1 = [];
+const johnLennonFacts = function (array) {
+  let counter = 0;
+  while (counter < array.length){
+    let curElement = array[counter];
+    curElement = (curElement + '!!!')
+    result1.push(curElement)
+    counter++;
+  }
+  console.log(result1);
+}
+johnLennonFacts()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
